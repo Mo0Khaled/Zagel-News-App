c30dc38c5d2aab69325342b06ce3b44dccfeceaf
mo <mohamedkx77@gmail.com>
2022-04-10 02:48:24 +0200

    created article entity

---
+++ lib/features/news/domain/entities/article_entity.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class ArticleEntity extends Equatable {
+  final String author;
+  final String title;
+  final String description;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  const ArticleEntity({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  @override
+  List<Object?> get props => [author, title, description, urlToImage, publishedAt, content];
+}
